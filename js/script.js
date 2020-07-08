de99b6a1e8d9748038fd3edb95969384cc6b52cb
grpargeter <pargetergregr@johndeere.com>
2020-07-08 13:59:30 -0500

    Player Change

--- js/script.js
+++ js/script.js
@@ -7,12 +7,31 @@ function playerSelection() {
   //statusDisplay.innerHTML = currentPlayerTurn()
 }
 
-playerSelection();
 //console.log(currentPlayer);
 const cellElements = document.querySelectorAll(".cell");
 cellElements.forEach((cell) => {
   cell.addEventListener("click", handleClick, { once: true });
 });
-function handleClick(e) {
-  console.log("click");
+let gameResults = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+
+function handleClick(clickedCellEvent) {
+  const clickedCell = clickedCellEvent.target;
+  const clickedCellIndex = clickedCell.getAttribute("data-cell-index");
+  console.log(clickedCellIndex);
+  if (currentPlayer === "Red") {
+    clickedCell.classList.add("red");
+  } else {
+    clickedCell.classList.add("blue");
+  }
+  playerSelection();
 }
+console.table(gameResults);
+console.log(cellElements);
+// cellElements.addEventListener("click", setColor);
+// function setColor() {
+//   cellElements.setAttribute("style", "background-color:red");
+// }
