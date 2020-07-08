a86bb05b29df112c1903f8f1e53c7c77e9dbba92
grpargeter <pargetergregr@johndeere.com>
2020-07-08 17:22:52 -0500

    Game results logic working

--- js/script.js
+++ js/script.js
@@ -1,37 +1,104 @@
-//console.log("I am here!");
+// console.log("script is working");
 
-//player1 always first and is red
+//player 1 always starts first and is red
+
 let currentPlayer = "Red";
-function playerSelection() {
-  currentPlayer = currentPlayer === "Red" ? "Blue" : "Red";
-  //statusDisplay.innerHTML = currentPlayerTurn()
-}
 
-//console.log(currentPlayer);
+// playerSelection();
+
+console.log(currentPlayer);
+
 const cellElements = document.querySelectorAll(".cell");
 cellElements.forEach((cell) => {
   cell.addEventListener("click", handleClick, { once: true });
 });
+
 let gameResults = [
   ["", "", ""],
   ["", "", ""],
   ["", "", ""],
 ];
+//console.log(gameResults);
 
 function handleClick(clickedCellEvent) {
   const clickedCell = clickedCellEvent.target;
   const clickedCellIndex = clickedCell.getAttribute("data-cell-index");
-  console.log(clickedCellIndex);
+
+  //   console.log(clickedCellIndex);
   if (currentPlayer === "Red") {
     clickedCell.classList.add("red");
   } else {
     clickedCell.classList.add("blue");
   }
+  arrayIndex = clickedCellIndex.split("");
+  //   console.log(arrayIndex);
+  gameResults[arrayIndex[0]][arrayIndex[1]] = currentPlayer;
+  winLogic();
+  //   console.log(gameResults);
+  console.table(gameResults);
   playerSelection();
+  //   console.log(document.getElementById.value);
 }
-console.table(gameResults);
-console.log(cellElements);
-// cellElements.addEventListener("click", setColor);
-// function setColor() {
-//   cellElements.setAttribute("style", "background-color:red");
-// }
+
+//console.log(cellElements);
+
+function playerSelection() {
+  currentPlayer = currentPlayer === "Red" ? "Blue" : "Red";
+  //statusDisplay.innerHTML = currentPlayerTurn();
+}
+function winLogic() {
+  console.log(gameResults[0][0]);
+  console.log(gameResults[0][1]);
+  console.log(gameResults[0][2]);
+  if (
+    gameResults[0][0] === gameResults[0][1] &&
+    gameResults[0][1] === gameResults[0][2] &&
+    gameResults[0][0] !== ""
+  ) {
+    console.log(`${currentPlayer} is the winner on row 1!`);
+  } else if (
+    gameResults[1][0] === gameResults[1][1] &&
+    gameResults[1][1] === gameResults[1][2] &&
+    gameResults[1][0] !== ""
+  ) {
+    console.log(`${currentPlayer} is the winner on row 2!`);
+  } else if (
+    gameResults[2][0] === gameResults[2][1] &&
+    gameResults[2][1] === gameResults[2][2] &&
+    gameResults[2][0] !== ""
+  ) {
+    console.log(`${currentPlayer} is the winner on row 3!`);
+  } else if (
+    gameResults[0][0] === gameResults[1][0] &&
+    gameResults[1][0] === gameResults[2][0] &&
+    gameResults[0][0] !== ""
+  ) {
+    console.log(`${currentPlayer} is the winner on column 1!`);
+  } else if (
+    gameResults[0][1] === gameResults[1][1] &&
+    gameResults[1][1] === gameResults[2][1] &&
+    gameResults[0][1] !== ""
+  ) {
+    console.log(`${currentPlayer} is the winner on column 2!`);
+  } else if (
+    gameResults[0][2] === gameResults[1][2] &&
+    gameResults[1][2] === gameResults[2][2] &&
+    gameResults[0][2] !== ""
+  ) {
+    console.log(`${currentPlayer} is the winner on column 3!`);
+  } else if (
+    gameResults[0][0] === gameResults[1][1] &&
+    gameResults[1][1] === gameResults[2][2] &&
+    gameResults[0][0] !== ""
+  ) {
+    console.log(`${currentPlayer} is the winner on diagonal left!`);
+  } else if (
+    gameResults[0][2] === gameResults[1][1] &&
+    gameResults[1][1] === gameResults[2][0] &&
+    gameResults[0][2] !== ""
+  ) {
+    console.log(`${currentPlayer} is the winner on diagonal right!`);
+  } else {
+    console.log("no winner yet");
+  }
+}
